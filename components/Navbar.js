6a8a8c10915f671ab76ca036d50b0f9de23507e1
Ayisha Alli <ayisha.alli@playtech.com>
2022-06-10 13:03:53 +0100

    started styling first page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Navbar() {
   const nav = ["HOME", "ABOUT", "SHOP", "FLOWERS", "COFFEE"];
-  const [likes, setLikes] = useState(0);
-  function handleClick() {
-    setLikes(likes + 1);
-  }
+  // const [likes, setLikes] = useState(0);
+  // function handleClick() {
+  //   setLikes(likes + 1);
+  // }
 
   return (
     <>
-      <ul>
-        {nav.map((nav) => (
-          <li ><a href="#" key={nav}>{nav}</a></li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Like({likes})</button>
+      <div className="nav">
+        <ul className="nav-links">
+          {nav.map((nav) => (
+            <li key={nav} className="nav-link">
+              <a href="#">{nav}</a>
+            </li>
+          ))}
+        </ul>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-basket3-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z" />
+        </svg>
+      </div>
+
+      {/* <button onClick={handleClick}>Like({likes})</button> */}
     </>
   );
 }
--- components/Top.js
+++ components/Top.js
@@ -1,4 +1,8 @@
+import Image from "next/image";
 import React from "react";
+
+import openingPlantImg from '../stock_images/plants/plantone-bg.png'
+
 import { Col, Container, Row } from "react-bootstrap";
 
 
@@ -9,13 +13,13 @@ export default function Home() {
     <>
       <div className="background"></div>
       <Container className="container">
-        <Row>
-          <Col xs={12} md={6}>
+        <Row className="hero-sec">
+          <Col xs={12} md={6} className="hero-left">
                 <h1 className="logo">PlantShed</h1>
                 <p>Plants, and <span>Coffee, What more could you ask for?</span></p>
           </Col>
           <Col xs={12} md={6}>
-              <img className="plant-img" src="/../stock_images/plantone-bg"></img>
+              <Image className="plant-img" alt="plant in pot" src={openingPlantImg} height={400} width={600}></Image>
           </Col>
         </Row>
       </Container>
